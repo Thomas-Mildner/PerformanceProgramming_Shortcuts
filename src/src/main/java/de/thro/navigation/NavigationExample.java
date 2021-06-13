3aba3f2eeee4f869c12aab353e1db52b69795405
Thomas Mildner <thomas.mildner@media-concept.com>
2021-06-13 19:28:57 +0200

    - add navigation comments

--- src/src/main/java/de/thro/navigation/NavigationExample.java
+++ src/src/main/java/de/thro/navigation/NavigationExample.java
@@ -2,4 +2,5 @@ package de.thro.navigation;
 
 public class NavigationExample
 {
+    /* ALT + Pfeiltasten */
 }
--- src/src/main/java/de/thro/refactoring/RefactoringExample.java
+++ src/src/main/java/de/thro/refactoring/RefactoringExample.java
@@ -1,10 +1,12 @@
 package de.thro.refactoring;
 
+/* CTRL + ALT + M  --> Extract Method*/
 public class RefactoringExample
 {
 
     public static void main(String[] args)
     {
+        /* Exercise: Extract Logic in Method*/
         var a = new TestA();
         a.printTest();
     }
